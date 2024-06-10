6ab1db591b198414ab62a3777ce6516113604f79
Manba Chauhan <chauhanmanba@gmail.com>
2024-06-10 18:37:29 +0530

    changes

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -25,60 +25,68 @@ const Section1 = () => {
       >
         <Box sx={{ px: { xs: 2, sm: 5, md: 20 }, pb: 25, pt: 2 }}>
           <Box
-            sx={{ 
-              ml: { xs: 2, sm: 7, md: 50, lg: 70 , xl:100,}, 
-              mr:2,
-              pr:{xs:4,sm:5,xl:10},
-              lineClamp: 10, py: 2,mt:4, 
-               letterSpacing:2,
-               width:{xs:"100%",sm:"80%",md:"58%",lg:"60%" ,xl:"48%"},
-               textAlign:"justify"
+            sx={{
+              ml: { xs: 2, sm: 7, md: 50, lg:70,xl:100},
+              mr: 2,
+              // pr:{xs:4,sm:5,xl:10},
+              lineClamp: 10,
+              py: 2,
+              mt: 4,
+              letterSpacing: 2,
+              maxwidth: "100%",
+              textAlign: "justify",
               //  float:"right"
-             }}
+            }}
           >
-            <Typography
-              variant="h1"
-              color="#fff"
-              component="div"
-              sx={{ fontWeight: "bold", fontSize: { xs: "50px" } }}
-            >
-              health
-            </Typography>
-            <Typography
-              variant="h5"
-              color="#fff"
-              component="div"
-              sx={{ fontWeight: "bold", fontSize: "16px" }}
-            >
-              Time to Get Healthy & Active
-            </Typography>
-            <Typography
-              variant="p"
-              color="#fff"
-              component={Box}
-              sx={{ mt: 2,  }}
-
-            >
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum.
-            </Typography>
-            <Button
-              variant="contained"
+            <Box
               sx={{
-                mt: 6,
-                bgcolor: "secondary.main",
-                px: 5,
-                py: 1.5,
-                color: "secondary.contrastText",
-                fontSize: "18px",
-                letterSpacing: 3,
-                textWrap: "nowrap",
+                width: {xs:"100%",sm:"100%", md:"100%",xl:"60%"},
+
               }}
             >
-              Read More
-            </Button>
+              <Typography
+                variant="h1"
+                color="#fff"
+                component="div"
+                sx={{ fontWeight: "bold", fontSize: { xs: "50px" } }}
+              >
+                health
+              </Typography>
+              <Typography
+                variant="h5"
+                color="#fff"
+                component="div"
+                sx={{ fontWeight: "bold", fontSize: "16px" }}
+              >
+                Time to Get Healthy & Active
+              </Typography>
+              <Typography
+                variant="p"
+                color="#fff"
+                component={Box}
+                sx={{ mt: 2, width: { xs: "100%", sm: "100%" } }}
+              >
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+              </Typography>
+              <Button
+                variant="contained"
+                sx={{
+                  mt: 5,
+                  bgcolor: "secondary.main",
+                  px: 5,
+                  py: 1.5,
+                  color: "secondary.contrastText",
+                  fontSize: "18px",
+                  letterSpacing: 3,
+                  textWrap: "nowrap",
+                }}
+              >
+                Read More
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Box>
